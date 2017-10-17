47a0c495b048cd381035bc3756656045325e144b
j2dunham <jmdunham20@gmail.com>
2017-10-17 07:43:22 -0400

    Updated to show git branch if it exists

--- Profile.ps1
+++ Profile.ps1
@@ -1,5 +1,14 @@
- function prompt {
-     $color = Get-Random -Min 1 -Max 16
-     Write-Host ("PS " + $(Get-Location))  -ForegroundColor $color
-     "--> "
- }
+function prompt
+{
+	 $color = Get-Random -Min 1 -Max 16
+	 $identity = [Security.Principal.WindowsIdentity]::GetCurrent()  
+	 $branch = (git rev-parse --abbrev-ref HEAD)
+     Write-Host ("<-- " + $(Get-Location) +" -->") -ForegroundColor "DarkGreen"
+	 if($branch)
+	 {
+		Write-Host ("~ "+$branch+" ~ ") -ForegroundColor "Red" -NoNewline
+	 }
+	 
+     Write-Host ("[" + $identity.Name +"]: ") -ForegroundColor "DarkCyan" -NoNewline
+     return " "
+}
